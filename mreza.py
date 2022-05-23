65b87ff4a645b98a310b6cb7411e106be72b0315
Someoneb100 <lawamulet@gmail.com>
2022-05-23 14:33:47 +0200

    mreza koja opterecuje memoriju

--- mreza.py
+++ mreza.py
@@ -2,6 +2,25 @@ from os import path, listdir
 from PIL import Image
 import numpy as np
 
+from tensorflow.keras.layers import GaussianNoise, Input, Rescaling, Reshape, Dropout, Flatten
+from tensorflow import clip_by_value
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Dense
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+
+import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
 def get_data(directory: str) -> "tuple[np.ndarray, np.ndarray]":
     ima = listdir(path.join(directory, "PNEUMONIA"))
     nema = listdir(path.join(directory, "NORMAL"))
@@ -14,11 +33,40 @@ def get_data(directory: str) -> "tuple[np.ndarray, np.ndarray]":
     for i, file in enumerate(nema):
         X[len(ima) + i] = np.asarray(Image.open(path.join(directory, "NORMAL", file)))
         y[len(ima) + i] = 0
-    return X / 255.0, y
+    return X , y
+
+
+def make_model(input_shape, learning_rate):
+    input_layer = Input(input_shape)
+    rs = Rescaling(scale=1/255)(input_layer)
+    gauss = GaussianNoise(2/255)(rs)
+    clip_input = clip_by_value(gauss, 0, 1)
+    reshape_input = Reshape((input_shape[0], input_shape[1], 1))(clip_input)
+
+    c1 = Conv2D(32, (3,3), activation="relu")(reshape_input)
+    mp1 = MaxPool2D((3,3))(c1)
+    c2 = Conv2D(64, (4,4), activation="relu")(mp1)
+    mp2 = MaxPool2D((4,4))(c2)
+    c3 = Conv2D(64, (5,5), activation="relu")(mp2)
+    mp3 = MaxPool2D((5,5))(c3)
+    drop1 = Dropout(0.2)(mp3) 
+    flat = Flatten()(drop1)
+    
+    d1 = Dense(256, activation="relu")(flat)
+    d2 = Dense(128, activation="relu")(d1)
+    
+    drop2 = Dropout(0.3)(d2)
+    output_layer = Dense(1, activation="sigmoid")(drop2)
+    model = Model(inputs=[input_layer], outputs=[output_layer])
+    model.compile(loss="binary_crossentropy", optimizer=Adam(learning_rate=learning_rate))
+    return model
+
 
 if __name__ == "__main__":
-    directory = "../chest_xray_norm/val"
-    x, y = get_data(directory)
-    for red in x[0]:
-        print(red)
-    print(y)
+    directory_train = "../chest_xray_norm/train"
+    x_train, y_train = get_data(directory_train)
+    
+    model = make_model(x_train[0].shape, 0.1)
+    model.summary()
+
+    model.fit(x_train, y_train, epochs=5, batch_size=0, verbose=1, shuffle=True)
